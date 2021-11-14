4cd25362c1b33dc0cdcc45e638d9eb40f60a6da0
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2021-11-13 23:10:02 -0600

    etherscan for buyer and seller

--- scripts/salesHistory.js
+++ scripts/salesHistory.js
@@ -4,7 +4,8 @@ async function getSalesHistory() {
     let eventFilter = marketplace.filters.MiceBought();
     let events = await marketplace.queryFilter(eventFilter);
     const _baseImageURI = "https://raw.githubusercontent.com/jozanza/anonymice-images/main/";
-    const baseEtherscanLink = "https://rinkeby.etherscan.io/tx/";
+    const baseEtherscanLinkTx = "https://rinkeby.etherscan.io/tx/";
+    const baseEtherscanLinkAddress = "https://etherscan.io/address/";
 
     let _darkClass = getDarkMode();
 
@@ -26,9 +27,9 @@ async function getSalesHistory() {
         const fakeJSX = `<tr class="sale-row ${_darkClass}">
                         <td class="sold-mice-img-container ${_darkClass}"  onclick="showInfo(${tokenID})"><img src="${_baseImageURI}${tokenID}.png" loading="lazy" alt="" class="sold-mice-img ${_darkClass}"><br>#${tokenID}</td>
                         <td>${price}<span class="listing-eth-logo">Ξ</span></td>
-                        <td>${seller}</td>
-                        <td>${buyer}</td>
-                        <td><a href="${baseEtherscanLink}${txId}" id="${txId}" target="_blank" rel="noopener noreferrer">${timeDelta} <p style="display:inline;font-size: 1.5vw">⬈</p></a></td>
+                        <td><a href="${baseEtherscanLinkAddress}${event.args.seller}" target="_blank" rel="noopener noreferrer">${seller}<p style="display:inline;font-size: 1.5vw">⬈</p></a></td>
+                        <td><a href="${baseEtherscanLinkAddress}${event.args.buyer}" target="_blank" rel="noopener noreferrer">${buyer}<p style="display:inline;font-size: 1.5vw">⬈</p></a></td>
+                        <td><a href="${baseEtherscanLinkTx}${txId}" id="${txId}" target="_blank" rel="noopener noreferrer">${timeDelta} <p style="display:inline;font-size: 1.5vw">⬈</p></a></td>
                        </tr>`
         $("#mice-sales").append(fakeJSX);
     }
