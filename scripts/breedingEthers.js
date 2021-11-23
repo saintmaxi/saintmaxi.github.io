7dc016b9dc0d608e69f5c621b8bcc8fe0e8656b4
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2021-11-23 14:13:04 -0600

    edit msg, add link to metamask

--- scripts/breedingEthers.js
+++ scripts/breedingEthers.js
@@ -24,9 +24,8 @@ const cheethAbi = ()=>{
 };
 
 if (window.ethereum == undefined) {
-    displayErrorMessage('Please use a web3 enabled browser like Chrome/Firefox or the MetaMask mobile app to access this feature of Anonymice Marketplace.', false);
-    $("#error-popup p").append(`<br><a href="https://saintmaxi.github.io" class="contract-link ${getDarkMode()}" rel="noopener noreferrer">back</a>`);
-
+    displayErrorMessage('Use a web3 enabled browser with MetaMask or the MetaMask mobile app to access this feature of Anonymice Marketplace.', false);
+    $("#error-popup p").append(`<br><a href="https://saintmaxi.github.io" class="contract-link ${getDarkMode()}" rel="noopener noreferrer">Home</a>|<a href="https://metamask.app.link/dapp/saintmaxi.github.io/" class="contract-link ${getDarkMode()}" target="_blank" rel="noopener noreferrer">MetaMask</a>`);
 }
 const provider = new ethers.providers.Web3Provider(window.ethereum,"any");
 const signer = provider.getSigner();
