f4c4245835f1dbcdba3e4c2dafdd7be616d5795e
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2021-11-30 22:10:50 -0600

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,12 @@
 async function displayErrorMessage(message, timed=true) {
-    let darkClass = getDarkMode();
-    let fakeJSX = `<div class="${darkClass}" id="error-popup"><p>${message}</p></div>`;
-    $("#directory").append(fakeJSX);
-    if (timed) {
-        await sleep(2500);
-        $("#error-popup").remove();
+    if (!($("#error-popup").length)) {
+        let darkClass = getDarkMode();
+        let fakeJSX = `<div class="${darkClass}" id="error-popup"><p>${message}</p></div>`;
+        $("#directory").append(fakeJSX);
+        if (timed) {
+            await sleep(2500);
+            $("#error-popup").remove();
+        }
     }
 }
 
